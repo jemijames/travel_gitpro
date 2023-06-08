56d09f536fc9a95ca1c6f18c441c1591415c87bd
jemijames <jemijames217@gmail.com>
2023-06-09 00:46:50 +0530

    new

---
+++ travelmainnew/travelapp/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from.models import Place
+from.models import Feedback
+
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj1=Feedback.objects.all()
+    return render(request,"index.html",{'result':obj,'result1':obj1})
+# def addition(request):
+#     x=int(request.GET['num1'])
+#     y=int(request.GET['num2'])
+#     res=x+y
+#     return render(request,"result.html",{'result':res})
+# def contact(request):
+#     return HttpResponse("hello am contact")
